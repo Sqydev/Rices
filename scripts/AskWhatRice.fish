c7b9eef14c501f4da02609cdce879b0e40e01f50
Sqyd <wojkap10@proton.me>
2025-08-04 00:23:13 +0200

    Readme & Scripts

--- scripts/AskWhatRice.fish
+++ scripts/AskWhatRice.fish
@@ -23,7 +23,7 @@ else
 end
 
 # Do scripts
-set SCRIPT_URL "https://raw.githubusercontent.com/Sqydev/Rices/refs/heads/main/scripts/$RICE_NAME/fish/$OS.fish"
+set SCRIPT_URL "https://raw.githubusercontent.com/Sqydev/Rices/refs/heads/main/scripts/$RICE_NAME/Fish/$OS.fish"
 set SCRIPT_PATH ~/.config/hypr/temp/Instalation.fish
 
 curl -Ls $SCRIPT_URL -o $SCRIPT_PATH
