a0cd9670eb13ff1713863f40e7000891ded4d578
Sqyd <wojkap10@proton.me>
2025-08-03 22:26:43 +0200

    Scripts

--- scripts/Minimalistic/NixOs.bash
+++ scripts/Minimalistic/NixOs.bash
@@ -12,10 +12,8 @@ while true; do
 	read -p "> " yn
     case $yn in
         [1]* ) break;;
-        [2]* ) rm -rf ~/.config/hypr/temp
-			exit;;
-        * ) rm -rf ~/.config/hypr/temp 
-			exit;;
+        [2]* ) exit;;
+        * ) exit;;
     esac
 done
 
