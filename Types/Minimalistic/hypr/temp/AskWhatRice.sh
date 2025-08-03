0c766b24078c6df83198c2e2b42362472f1362ee
Sqyd <wojkap10@proton.me>
2025-08-03 21:58:48 +0200

    Scripts

--- Types/Minimalistic/hypr/temp/AskWhatRice.sh
+++ Types/Minimalistic/hypr/temp/AskWhatRice.sh
@@ -1,9 +1,29 @@
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
+echo "Select rice:"
+echo "1) Minimalistic"
+read -p "> " RICE
+
+case $RICE in
+    1)
+        RICE_ID="1"
+        ;;
+    *)
+        echo "NUHUH"
+        exit 1
+        ;;
+esac
+
+SHELL_NAME=$(basename "$SHELL")
+
+# Find what os
+if grep -qi "NixOs" /etc/os-release; then
+    OS="NixOs"
+elif grep -qi "Arch" /etc/os-release; then
+    OS="Arch"
 else
-    echo "No /etc/os-release"
+	echo "NUHUH"
+    exit 1
 fi
 
-if [ "$NAME" = "NixOS" ]; then
-	echo "UWU"
-fi
+echo $OS
+echo $RICE_ID
+echo $SHELL_NAME
--- scripts/StartInstalation.sh
+++ scripts/StartInstalation.sh
@@ -2,4 +2,4 @@ mkdir -p ~/.config/hypr/temp
 
 curl -L "https://raw.githubusercontent.com/Sqydev/Rices/refs/heads/main/scripts/AskWhatRice.sh" -o ~/.config/hypr/temp/AskWhatRice.sh
 
-$SHELL ~/.config/hypr/temp/AskWhatRice.sh
+bash ~/.config/hypr/temp/AskWhatRice.sh
