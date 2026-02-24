dd6e16605a5bd030d6ee82349a6d3fd66035f159
Sqydev <138494523+Sqydev@users.noreply.github.com>
2026-02-24 19:15:16 +0100

    ...

--- rices/Iceberg/scripts/launcher.bash
+++ rices/Iceberg/scripts/launcher.bash
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
-# Check what workspace
 current_ws=$(hyprctl activeworkspace -j | jq -r '.name')
+is_rofi_running=$(pgrep -x rofi)
 
-# If in special than only toggle
+# 1. If special and rofi -> kill and escape
 if [[ "$current_ws" == "special:control" ]]; then
+    if [[ -n "$is_rofi_running" ]]; then
+        pkill rofi
+    fi
     hyprctl dispatch togglespecialworkspace control
     exit 0
 fi
 
-hyprctl dispatch togglespecialworkspace control
+# 2. If rofi works -> kill it
+if [[ -n "$is_rofi_running" ]]; then
+    pkill rofi
+    exit 0
+fi
 
+# 3. Btop is dead -> do it
 if ! pgrep -x btop >/dev/null; then
     alacritty --class Btop -e btop &
 fi
 
-# No & becouse if rofi cloases then like it switches workspaces
+hyprctl dispatch togglespecialworkspace control
+
 rofi -show drun
 
 hyprctl dispatch togglespecialworkspace control
